1e45b786b113e735b6cb90c6c0016fc9ff812c6b
Spencer Williams <s@spencerenglish.com>
2013-09-18 18:31:56 -0700

    rails installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scrapbook::Application.config.secret_key_base = '7d747c9056cc77352e13514470fa0cffbc6c1d9022b6b9100233a6b08512766e4883d2d8b2facf5c59ce2ef6730177433f0303a8f57e7b4b8840e1216ed19b56'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Scrapbook::Application.config.session_store :cookie_store, key: '_scrapbook_session'
